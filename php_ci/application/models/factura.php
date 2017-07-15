605e71831c42dc5324d8a94c7c59e5c115e43d6f
Luis Lomeli <luism.lomeli@gmail.com>
2017-07-14 18:18:58 -0600

    Tomar en cuenta facturas version 3.2 y 3.3

--- php_ci/application/models/factura.php
+++ php_ci/application/models/factura.php
@@ -30,32 +30,39 @@ class Factura extends BaseModel {
 			return false;
 		}
 
-		// read emisro/receptor RFC from XML
-		foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Emisor') as $item) {
-			$emisor_rfc    = (string)$item['rfc'];
-			$emisor_nombre = (string)$item['nombre'];
+		// get the XML version
+		foreach ($xml->xpath('//cfdi:Comprobante') as $item) {
+			if ( ! empty((string)$item['version']) ) {
+				$version = (string)$item['version'];
+			} else if ( ! empty((string)$item['Version']) ) {
+				$version = (string)$item['Version'];
+			}
 		}
 
-		foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Receptor') as $item) {
-			$receptor_rfc    = (string)$item['rfc'];
-			$receptor_nombre = (string)$item['nombre'];
+
+		if ($version == '3.2') {
+			$xmlData = $this->getDataXML32($xml);
+		}
+
+		if ($version == '3.3') {
+			$xmlData = $this->getDataXML33($xml);
 		}
 
 		// check configuration RFC is equals to Emisor/Receptor
-		if ($config->rfc != $emisor_rfc && $config->rfc != $receptor_rfc) {
+		if ($config->rfc != $xmlData['emisor_rfc'] && $config->rfc != $xmlData['receptor_rfc']) {
 			$this->setError('El RFC del Emisor/Receptor del XML no coincide con el RFC de la configuracion');
 			return false;
 		}
 
 		// Factura is emited o received
-		if ($config->rfc == $emisor_rfc) {
+		if ($config->rfc == $xmlData['emisor_rfc']) {
 			$tipo   = 'E';
-			$rfc    = $receptor_rfc;
-			$nombre = $receptor_nombre;
+			$rfc    = $xmlData['receptor_rfc'];
+			$nombre = $xmlData['receptor_nombre'];
 		} else {
 			$tipo   = 'R';
-			$rfc    = $emisor_rfc;
-			$nombre = $emisor_nombre;
+			$rfc    = $xmlData['emisor_rfc'];
+			$nombre = $xmlData['emisor_nombre'];
 		}
 
 		// check if Cliente exists, or create it
@@ -83,19 +90,54 @@ class Factura extends BaseModel {
 			$cliente_id = $recs['data'][0]->id;
 		}
 
+		// save Factura
+		$data = array(
+			'id'         => 0,
+			'tipo'       => $tipo,
+			'cliente_id' => $cliente_id,
+			'rfc'        => $rfc,
+			'fecha'      => str_replace('T', ' ', $xmlData['fecha']),
+			'subtotal'   => $xmlData['subtotal'],
+			'iva'        => $xmlData['impuestos_trasladados'],
+			'total'      => $xmlData['total'],
+			'activo'     => 1,
+			'serie'      => $xmlData['serie'],
+			'folio'      => $xmlData['folio']
+		);
+
+		if (!$this->save($data, true)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private function getDataXML32($xml) {
+		$data = array();
+
+		// read emisor/receptor RFC from XML
+		foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Emisor') as $item) {
+			$data['emisor_rfc']    = (string)$item['rfc'];
+			$data['emisor_nombre'] = (string)$item['nombre'];
+		}
+
+		foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Receptor') as $item) {
+			$data['receptor_rfc']    = (string)$item['rfc'];
+			$data['receptor_nombre'] = (string)$item['nombre'];
+		}
+
 		// read more data from XML
 		foreach ($xml->xpath('//cfdi:Comprobante') as $item) {
-			$serie    = (string)$item['serie'];
-			$folio    = (string)$item['folio'];
-			$fecha    = (string)$item['fecha'];
-			$subtotal = (float)$item['subTotal'];
-			$total    = (float)$item['total'];
+			$data['serie']    = (string)$item['serie'];
+			$data['folio']    = (string)$item['folio'];
+			$data['fecha']    = (string)$item['fecha'];
+			$data['subtotal'] = (float)$item['subTotal'];
+			$data['total']    = (float)$item['total'];
 		}
 
 		foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Impuestos') as $item) {
-			$impuestos_trasladados = (float)$item['totalImpuestosTrasladados'];
+			$data['impuestos_trasladados'] = (float)$item['totalImpuestosTrasladados'];
 		}
-
 
 		// foreach ($xml->xpath('//cfdi:Comprobante') as $item) {
 		// 	var_dump((string)$item);
@@ -111,27 +153,51 @@ class Factura extends BaseModel {
 		// 	}
 		// }
 
+		return $data;
+	}
 
-		// save Factura
-		$data = array(
-			'id'         => 0,
-			'tipo'       => $tipo,
-			'cliente_id' => $cliente_id,
-			'rfc'        => $rfc,
-			'fecha'      => str_replace('T', ' ', $fecha),
-			'subtotal'   => $subtotal,
-			'iva'        => $impuestos_trasladados,
-			'total'      => $total,
-			'activo'     => 1,
-			'serie'      => $serie,
-			'folio'      => $folio
-		);
+	private function getDataXML33($xml) {
+		$data = array();
 
-		if (!$this->save($data, true)) {
-			return false;
+		// read emisor/receptor RFC from XML
+		foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Emisor') as $item) {
+			$data['emisor_rfc']    = (string)$item['Rfc'];
+			$data['emisor_nombre'] = (string)$item['Nombre'];
 		}
 
-		return true;
+		foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Receptor') as $item) {
+			$data['receptor_rfc']    = (string)$item['Rfc'];
+			$data['receptor_nombre'] = (string)$item['Nombre'];
+		}
+
+		// read more data from XML
+		foreach ($xml->xpath('//cfdi:Comprobante') as $item) {
+			$data['serie']    = (string)$item['Serie'];
+			$data['folio']    = (string)$item['Folio'];
+			$data['fecha']    = (string)$item['Fecha'];
+			$data['subtotal'] = (float)$item['SubTotal'];
+			$data['total']    = (float)$item['Total'];
+		}
+
+		foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Impuestos') as $item) {
+			$data['impuestos_trasladados'] = (float)$item['TotalImpuestosTrasladados'];
+		}
+
+		// foreach ($xml->xpath('//cfdi:Comprobante') as $item) {
+		// 	var_dump((string)$item);
+		// 	// $xml2 = new SimpleXmlIterator($item->asXML());
+		// 	// $xml2->registerXPathNamespace("cfdi", "http://www.w3.org/2005/Atom");
+		// 	// var_dump($xml2);
+		// }
+
+		// foreach ($xml->children('cfdi',true) as $item) {
+		// 	if ($item->getName() == 'Complemento') {
+		// 		$item->registerXPathNamespace('tfd',true);
+		// 		var_dump($item->attributes);
+		// 	}
+		// }
+
+		return $data;
 	}
 }
 
